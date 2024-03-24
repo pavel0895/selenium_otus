b19bef0693dc42c48cc2183d552f2640ae0ad105
pavel0895 <pavl171@yandex.ru>
2024-03-24 21:34:35 +0300

    Добавил режим headless

--- conftest.py
+++ conftest.py
@@ -6,24 +6,31 @@ from selenium.webdriver.firefox.options import Options as FirefoxOptions
 
 def pytest_addoption(parser):
     parser.addoption("--browser", default="chrome")
-    parser.addoption("--base_url", default="192.168.0.3:8081")
+    parser.addoption("--base_url", default="http://192.168.0.3:8081")
+    parser.addoption("--headless", action="store_true")
 
 
 @pytest.fixture()
 def browser(request):
     browser_name = request.config.getoption("--browser")
+    headless = request.config.getoption("--headless")
     base_url = request.config.getoption("--base_url")
     driver = None
 
     if browser_name == "chrome":
         options = ChromeOptions()
         options.add_argument('--ignore-certificate-errors')
+        if headless:
+            options.add_argument("--headless")
         driver = webdriver.Chrome(options=options)
     elif browser_name == "firefox":
         options = FirefoxOptions()
+        if headless:
+            options.add_argument("--headless")
         driver = webdriver.Firefox(options=options)
 
     driver.maximize_window()
+
     driver.base_url = base_url
 
-    driver.close()
+    return driver
