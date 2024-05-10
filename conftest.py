4b1b92a66aa90d8cce71fb60ff28afa0f2423f5c
pavel0895 <pavl171@yandex.ru>
2024-05-10 13:48:28 +0300

    Настроил Selenoid, добавил браузер MicrosoftEdge

--- conftest.py
+++ conftest.py
@@ -1,3 +1,5 @@
+import datetime
+import logging
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options as ChromeOptions
@@ -6,8 +8,15 @@ from selenium.webdriver.firefox.options import Options as FirefoxOptions
 
 def pytest_addoption(parser):
     parser.addoption("--browser", default="chrome")
-    parser.addoption("--base_url", default="http://192.168.0.3:8081")
+    parser.addoption("--base_url", default="http://192.168.0.4:8081")
     parser.addoption("--headless", action="store_true")
+    parser.addoption("--log_level", action="store", default="INFO")
+    parser.addoption("--executor", action="store", default="127.0.0.1")
+    parser.addoption("--mobile", action="store_true")
+    parser.addoption("--vnc", action="store_true")
+    parser.addoption("--logs", action="store_true")
+    parser.addoption("--video", action="store_true")
+    parser.addoption("--bv")
 
 
 @pytest.fixture()
@@ -15,7 +24,24 @@ def browser(request):
     browser_name = request.config.getoption("--browser")
     headless = request.config.getoption("--headless")
     base_url = request.config.getoption("--base_url")
+    log_level = request.config.getoption("--log_level")
+    executor = request.config.getoption("--executor")
+    vnc = request.config.getoption("--vnc")
+    version = request.config.getoption("--bv")
+    logs = request.config.getoption("--logs")
+    video = request.config.getoption("--video")
+    mobile = request.config.getoption("--mobile")
+
+    executor_url = f"http://{executor}:4444/wd/hub"
+
+    logger = logging.getLogger(request.node.name)
+    file_handler = logging.FileHandler(f'logs/{request.node.name}.log')
+    file_handler.setFormatter(logging.Formatter('%(levelname)s %(message)s'))
+    logger.addHandler(file_handler)
+    logger.setLevel(level=log_level)
     driver = None
+
+    logger.info("===> Test %s started at %s" % (request.node.name, datetime.datetime.now()))
 
     if browser_name == "chrome":
         options = ChromeOptions()
@@ -29,8 +55,43 @@ def browser(request):
             options.add_argument("--headless")
         driver = webdriver.Firefox(options=options)
 
+    elif browser_name == "edge":
+        options = M()
+        if headless:
+            options.add_argument("--headless")
+        driver = webdriver.Firefox(options=options)
+
+    caps = {
+        "browserName": browser_name,
+        "browserVersion": version,
+        "selenoid:options": {
+            "name": request.node.name
+        },
+        "acceptInsecureCerts": True,
+    }
+
+    for k, v in caps.items():
+        options.set_capability(k, v)
+
+    driver = webdriver.Remote(
+        command_executor=executor_url,
+        options=options
+    )
+
     driver.maximize_window()
-
     driver.base_url = base_url
+    driver.log_level = log_level
+    driver.logger = logger
+    driver.test_name = request.node.name
 
+    logger.info("Browser %s started" % browser_name)
+
+    if not mobile:
+        driver.maximize_window()
+
+    def fin():
+        driver.quit()
+        logger.info("===> Test %s finished at %s" % (request.node.name, datetime.datetime.now()))
+
+    request.addfinalizer(fin)
     return driver
